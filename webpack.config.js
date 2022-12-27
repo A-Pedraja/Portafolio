0a6e12f5712a71259a7c42272114720d4e8d10dc
Andres Pedraja <pedraja.andres7@gmail.com>
2022-12-26 19:21:47 -0500

    incluye webpack2 :D

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
@@ -43,7 +42,7 @@ module.exports = {
     optimization: {
         minimizer: [
             new CssMinimizerPlugin(),
-            new TerserPlugin()
+            new TerserPlugin(),
         ]
     },
 
